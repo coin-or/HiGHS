a43ec59d27831b1050599200088fcf33adb33e26
JAJHall <jajhall@ed.ac.uk>
2025-11-02 14:42:40 +0000

    Try again

--- highs/ipm/ipx/timer.cc
+++ highs/ipm/ipx/timer.cc
@@ -4,7 +4,9 @@ namespace ipx {
 
 Timer::Timer(const double offset)
     : offset_(offset) {
+  if (offset_) printf("Timer::Timer offset_ = %g\n", offset_);
     Reset(true);
+  if (offset_) printf("Timer::Timer offset_ = %g\n", offset_);
 }
 
 double Timer::Elapsed() const {
@@ -12,10 +14,14 @@ double Timer::Elapsed() const {
 }
 
 void Timer::Reset(const bool first) {
+    using namespace std::chrono;
     double prev_offset = offset_;
-    if (!first) offset_ -= this->read();
+    if (!first)
+      offset_ -= duration_cast<duration<double>>(t0_.time_since_epoch()).count();    
     t0_ = tic();
-    if (!first) offset_ += this->read();
+    if (!first) 
+      offset_ += duration_cast<duration<double>>(t0_.time_since_epoch()).count();
+    if (offset_) printf("Timer::Reset offset goes from %g to %g\n", prev_offset, offset_);
 }
 
 Timer::TimePoint Timer::tic() {
--- highs/ipm/ipx/timer.h
+++ highs/ipm/ipx/timer.h
@@ -4,7 +4,6 @@
 #include <chrono>
 
 namespace ipx {
-using namespace std::chrono;
 
 class Timer {
 public:
@@ -13,11 +12,10 @@ public:
     void Reset(const bool first = false);
 
 private:
-    typedef time_point<high_resolution_clock>
+    typedef std::chrono::time_point<std::chrono::high_resolution_clock>
         TimePoint;
     static TimePoint tic();
     static double toc(TimePoint start);
-    static double read() { return duration_cast<duration<double>>(high_resolution_clock::now().time_since_epoch()).count(); }
     TimePoint t0_;
 public:
     double offset_;
