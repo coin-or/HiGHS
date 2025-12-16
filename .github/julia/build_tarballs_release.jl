8fc764ac498bd6c231be293ee4afbea0c195c743
Ivet Galabova <galabovaa@gmail.com>
2025-12-16 14:31:38 +0200

    cpack

--- .github/julia/build_tarballs_release.jl
+++ .github/julia/build_tarballs_release.jl
@@ -9,7 +9,13 @@ using BinaryBuilder, Pkg
 name = "HiGHS"
 version = VersionNumber(ENV["HIGHS_RELEASE"])
 
-sources = [GitSource(ENV["HIGHS_URL"], ENV["HIGHS_COMMIT"])]
+sources = [GitSource(ENV["HIGHS_URL"], ENV["HIGHS_COMMIT"])
+    # ArchiveSource("https://github.com/xianyi/OpenBLAS/releases/download/v0.3.21/OpenBLAS-0.3.21.tar.gz",
+    #               "f36ba3d7a60e7c8bcc54cd9aaa9b1223dd42eaf02c811791c37e8ca707c241ca")
+    ArchiveSource(
+        "https://github.com/OpenMathLib/OpenBLAS/releases/download/v0.3.30/OpenBLAS-0.3.30.tar.gz",
+                   "27342cff518646afb4c2b976d809102e368957974c250a25ccc965e53063c95d")
+    ]
 
 script = raw"""
 export BUILD_SHARED="OFF"
@@ -28,15 +34,33 @@ if [[ "${BUILD_SHARED}" == "OFF" ]] && [[ "${target}" == *-mingw* ]]; then
     export CXXFLAGS="-static"
 fi
 
+if [[ "${target}" == *-darwin-* ]]; then
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=${BUILD_SHARED} \
     -DZLIB_USE_STATIC_LIBS=${BUILD_STATIC} \
     -DHIPO=ON \
     -DBUILD_STATIC_EXE=ON \
-    -DBLAS_LIBRARIES="${libdir}/libopenblas.${dlext}" \
     ..
+else
+cd $WORKSPACE/srcdir/OpenBLAS*
+make DYNAMIC_ARCH=1 NO_SHARED=1 USE_OPENMP=0 NUM_THREADS=64 BINARY=64 -j${nproc}
+make install PREFIX=${prefix} NO_SHARED=1
+
+cd $WORKSPACE/srcdir/HiGHS
+apk del cmake
+mkdir -p build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DZLIB_USE_STATIC_LIBS=${BUILD_STATIC} \
+    -DHIPO=ON \
+    -DBUILD_STATIC_EXE=ON \
+    -DBLAS_LIBRARIES="${prefix}/lib/libopenblas.a" \
+    ..
+fi
 
 if [[ "${target}" == *-linux-* ]]; then
         make -j ${nproc}
@@ -63,7 +87,6 @@ platforms = expand_cxxstring_abis(platforms)
 dependencies = [
     Dependency("CompilerSupportLibraries_jll"),
     Dependency("Zlib_jll"),
-    Dependency("OpenBLAS32_jll"),
     HostBuildDependency(PackageSpec(; name="CMake_jll")),
 ]
 
