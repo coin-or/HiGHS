9758ca38ba48379eedcccf3a8d8dc09a21824697
fwesselm <fwesselm@mathworks.com>
2026-02-12 14:44:44 +0100

    Minor change to enumeration presolve

--- highs/mip/HighsCliqueTable.h
+++ highs/mip/HighsCliqueTable.h
@@ -58,6 +58,7 @@ class HighsCliqueTable {
     HighsInt origin;
     HighsInt numZeroFixed;
     bool equality;
+    HighsInt numActive() const { return end - start - numZeroFixed; }
   };
 
   struct Substitution {
@@ -109,6 +110,13 @@ class HighsCliqueTable {
 
   HighsInt runCliqueSubsumption(const HighsDomain& globaldom,
                                 std::vector<CliqueVar>& clique);
+
+  void cliqueSubsumption(const std::vector<CliqueVar>& clique, bool& redundant,
+                         HighsInt& dominatingOrigin,
+                         std::vector<HighsInt>& cliquesToRemove);
+
+  void collectCliques(const std::vector<CliqueVar>& clique);
+
   struct BronKerboschData {
     const std::vector<double>& sol;
     std::vector<CliqueVar> P;
